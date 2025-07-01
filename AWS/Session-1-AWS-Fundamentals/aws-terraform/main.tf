dc069f376c7cae1103c09d864700e9dd3320045d
Necip Ulusoy <113590193+necipulusoy@users.noreply.github.com>
2025-07-01 10:23:44 +0300

    Update main.tf

--- AWS/Session-1-AWS-Fundamentals/aws-terraform/main.tf
+++ AWS/Session-1-AWS-Fundamentals/aws-terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 # Define the EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (use a valid AMI ID for your region)
+  ami           = "ami-05ffe3c48a9991133" # Amazon Linux 2 AMI (use a valid AMI ID for your region)
   instance_type = "t2.micro"              # Free-tier eligible instance type
 
   tags = {
