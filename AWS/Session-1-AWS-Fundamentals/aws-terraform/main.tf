ead1f17a60522ec2fa9db34608d5a0c0424eb633
Samet Ustaoglu <samet@hepapi.com>
2025-09-26 14:53:11 +0200

    aws terraform initial commit

--- AWS/Session-1-AWS-Fundamentals/aws-terraform/main.tf
+++ AWS/Session-1-AWS-Fundamentals/aws-terraform/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 # Define the EC2 instance
 resource "aws_instance" "example" {
   ami           = "ami-0229b8f55e5178b65" # Amazon Linux 2 AMI (use a valid AMI ID for your region)
-  instance_type = "t2.micro"              # Free-tier eligible instance type
+  instance_type = "t3.micro"              # Free-tier eligible instance type
 
   tags = {
     Name = "terraform-instance"
